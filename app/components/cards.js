f49b7ecf445e7e2ce81cf4af16c56e3b24040b8d
Hoang Nghi <hoangnghinguyen17@gmail.com>
2025-12-01 15:58:20 +0700

    complete mouse evet (4)

--- app/components/cards.js
+++ app/components/cards.js
@@ -7,13 +7,14 @@ function Cards({cardsContent = []}){
     const [change, setChange] = useState(0);         //Độ biến thiên khi vuốt
     const [currentX, setCurrentX] = useState(0);     //Tọa độ X hiện tại sau khi vuốt
     const start = useRef(null);                      //Tọa độ khi bắt đầu vuốt
-    const transition = useRef('none');               //Xác định Transition để tạo hiệu ứng khi vuốt hoặc khi ngừng vuốt
+    const [transition, setTransition] = useState('none');               //Xác định Transition để tạo hiệu ứng khi vuốt hoặc khi ngừng vuốt
     const STEP = 400                                 //Tọa độ lướt phù hợp với chiều rộng thẻ card.
 
     //Sự kiện cho Mobile (Touch start/end/move)
     //Xử lý sự kiện chạm
     function handleTouchStart(e){
         e.preventDefault()
+        setTransition('transform 0s')
         if(start.current) return
         start.current = e.touches[0].clientX;
     }
@@ -22,24 +23,27 @@ function Cards({cardsContent = []}){
     function handleTouchMove(e){
         if (!start.current) return
         e.preventDefault();
-        transition.current = 'transform 0s';
         const currentChange = start.current - e.touches[0].clientX
-        if(Math.abs(currentChange)< 5) return 
+        if(Math.abs(currentChange)< 5) {
+            start.current = null
+            return 
+        }
         setChange(currentChange)
     }
 
     //Xử lý sự kiện ngừng chạm
     function handleTouchEnd(){
-        transition.current = '0.5s'
+        if(!start.current) return
+        setTransition('transform 0.5s')
         setCurrentX((i) => {
-            if(Math.abs(change) >= (currentX + i)/5) i += change >= 0 ? -STEP : STEP
+            if(Math.abs(change) >= (STEP)/5) i += change >= 0 ? -STEP : STEP
             if (i <= -400) i = -STEP
             if (i >= 400) i = STEP
             return i
         })
         setChange(0)
         start.current = null
-        setTimeout(() => (transition.current = 'none'), 500)
+        setTimeout(() => (setTransition('transform 0s')), 500)
     }
 
 
@@ -47,6 +51,7 @@ function Cards({cardsContent = []}){
     //Xử lý sự kiện nhấn xuống
     function handleMouseDown(e){
         e.preventDefault() //Tránh bôi đen nội dung khi trượt
+        setTransition("transform 0s")
         if(e.button != 0 || start.current) return   //return nếu không phải click hoặc không phải giao diện Mobile
         start.current = e.clientX
         return
@@ -62,13 +67,14 @@ function Cards({cardsContent = []}){
         setChange(distance)
     }
     return(
-        <div className="cards" style={{transform: `translateX(${currentX - change}px)`, transition: transition.current}}
+        <div className="cards" style={{transform: `translateX(${currentX - change}px)`, transition: transition}}
                 onTouchStart= {handleTouchStart}
                 onTouchMove= {handleTouchMove}
                 onTouchEnd= {handleTouchEnd}
                 onMouseDown = {handleMouseDown}
                 onMouseMove = {handleMouseMove} 
-                onMouseUp = {handleTouchEnd}>
+                onMouseUp = {handleTouchEnd}
+                onMouseLeave={handleTouchEnd}>
           {cardsContent.map((c,i)=> (
             <div className='card' key={i}>
                 <h1 className='cardTitle'>{c.title}</h1>
