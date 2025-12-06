c5fd9c6e104c0ad4e868e65e0a8fc9798ee62511
Hoang Nghi <hoangnghinguyen17@gmail.com>
2025-12-06 20:39:45 +0700

    design login page

--- app/login/page.js
+++ app/login/page.js
@@ -5,8 +5,14 @@ export default function login(){
         <div className='loginContainer'>
             <LoginHeader className='loginHeader'></LoginHeader>
             <div className='loginForm'>
-                
+                <h2 className='loginTitle'>LOG IN</h2>
+                <label><b>Tên đăng nhập:</b></label>
+                <input type='text' className='input' id='userName' placeholder='Tên đăng nhập'></input>
+                <label><b>Mật khẩu:</b></label>
+                <input type='password' className='input' id='passWord' placeholder='Mật khẩu'></input>
+                <button className='loginButton'><b>Đăng nhập</b></button>
             </div>
+            <img className='loginBackground' src='/loginbackground.png'></img>
         </div>
     )
 }
