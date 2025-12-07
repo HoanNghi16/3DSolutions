4d2cb6a11744f04b2f209da4d3e0b0943a32e82a
Hoang Nghi <hoangnghinguyen17@gmail.com>
2025-12-07 14:37:03 +0700

    fix useSearchParams hook

--- app/login/page.js
+++ app/login/page.js
@@ -1,11 +1,12 @@
 import './login.css'
 import LoginHeader from '../components/loginHeader'
 import LoginForm from '../components/loginForm'
+import { Suspense } from 'react'
 export default function login(){
     return (
         <div className='loginContainer'>
             <LoginHeader className='loginHeader'></LoginHeader>
-            <LoginForm></LoginForm>
+            <Suspense fallback={<div>loading login...</div>}><LoginForm></LoginForm></Suspense>
             <img className='loginBackground' src='/loginbackground.png'></img>
         </div>
     )
