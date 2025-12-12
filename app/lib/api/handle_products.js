d84e6caf5b7b7719979bd3d0e4a564967d1f06a3
Hoang Nghi <hoangnghinguyen17@gmail.com>
2025-12-12 09:10:07 +0700

    save

--- app/lib/api/handle_products.js
+++ app/lib/api/handle_products.js
@@ -1,3 +1,5 @@
-async function get_products(){
-
+export async function get_products(){
+    const api_url = process.env.NEXT_PUBLIC_API_URL + process.env.NEXT_PUBLIC_PRODUCTS_APPLICATION + process.env.NEXT_PUBLIC_PRODUCTS
+    const res = await fetch(api_url).then(res => res.json())
+    return res
 }
--- app/main/products/page.js
+++ app/main/products/page.js
@@ -1,17 +1,18 @@
 // app/products/page.js
 import './products.css'
-import handle_products from "../../lib/api/handle_products"
+import {get_products} from "../../lib/api/handle_products"
 
 export default async function ProductsPage() {
   // const api_url = process.env.NEXT_PUBLIC_API_URL + process.env.NEXT_PUBLIC_PRODUCTS_APPLICATION
   // const res = await fetch(api_url)
   // const data = await res.json()
-  // const message = (<span className="mess">Không có sản phẩm nào để hiển thị.</span>)
-
+  const message = (<span className="mess">Không có sản phẩm nào để hiển thị.</span>)
+  const data = await get_products();
+  console.log(data)
   return (
     <div style={{ padding: 20 }}>
       <section>
-        {/* {data == "None"? message: null} */}
+        {data == "None"? message: typeof(data)}
       </section>
     </div>
   );
