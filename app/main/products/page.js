6cd16fb827ead063ff6a5fc4df5ed46bbd71b7de
Hoang Nghi <hoangnghinguyen17@gmail.com>
2025-12-12 12:16:35 +0700

    add

--- app/main/products/page.js
+++ app/main/products/page.js
@@ -1,6 +1,7 @@
 // app/products/page.js
 import './products.css'
 import {get_products} from "../../lib/api/handle_products"
+import { error } from 'console';
 
 export default async function ProductsPage() {
   // const api_url = process.env.NEXT_PUBLIC_API_URL + process.env.NEXT_PUBLIC_PRODUCTS_APPLICATION
@@ -11,8 +12,16 @@ export default async function ProductsPage() {
   console.log(data)
   return (
     <div style={{ padding: 20 }}>
-      <section>
-        {data == "None"? message: typeof(data)}
+      <section className='show_products'>
+        {data == "None"? message:  data.map((item, i) => {
+          return (
+            <div key={i} className='product_card'>
+              <p className='product_name'>{item.name}</p>
+              <p className='product_price'>{item.unit_price}</p>
+              <p className='product_des'>{item.description}</p>
+            </div>
+          )
+        })}
       </section>
     </div>
   );
