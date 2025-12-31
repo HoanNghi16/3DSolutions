878b287d1670cff04558382655deff91b176a371
Hoang Nghi <hoangnghinguyen17@gmail.com>
2025-12-31 13:21:25 +0700

    save

--- app/components/products/productCard.js
+++ app/components/products/productCard.js
@@ -8,5 +8,11 @@ export default function ProductCard({item}){
             <img src={item.thumbnail} className='product_thumb'></img>
             <h5 className='product_name'>{item.name}</h5>
             <p className='product_price'>{ShowPriceFormat(item.unit_price)}</p>
+            <div className='product_buttons'>
+                <button className='buy_now'><b>Mua ngay</b></button>
+                <button className='add_to_cart'>
+                    <img src='/cart.png' loading='lazy' className='cart_icon'></img>
+                </button>
+            </div>
         </a>)
 }
