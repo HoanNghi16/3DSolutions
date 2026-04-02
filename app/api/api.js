92e8476662b2281dbb603573296636518140b9e5
Hoang Nghi <hoangnghinguyen17@gmail.com>
2026-04-02 19:05:48 +0700

    aishhhh

--- app/api/api.js
+++ app/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env?.NEXT_PUBLIC_BASE_URL ??"http://localhost:3000"
+const BASE_URL = process.env?.NEXT_PUBLIC_BASE_URL ?? process.env.BASE_URL
 
 export const postLogout = async () =>{
     const res = await fetch(`${BASE_URL}/api/auth/logout/`, {
